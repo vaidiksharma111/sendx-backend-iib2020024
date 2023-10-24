a713117c858f6186db533c45bbd94b24d1f31724
Vaidik Sharma <chrisharry1281@gmail.com>
2023-10-24 23:11:58 +0530

    javascript changes

--- javascript/configuration.js
+++ javascript/configuration.js
@@ -1,62 +1,55 @@
-async function updateConfig() {
-    const maxCrawls = document.getElementById('maxCrawls').value;
-    const numWorkers = document.getElementById('numWorkers').value;
+async function updateConfiguration() {
+    const maxCrawlsValue = document.getElementById('maxCrawls').value;
+    const numWorkersValue = document.getElementById('numWorkers').value;
 
     // Check if the input fields are not empty and are valid numbers
-    if (!numWorkers && !maxCrawls) {
+    if (!numWorkersValue && !maxCrawlsValue) {
         alert("Please enter valid numbers in both fields.");
         return;
     }
 
     await fetch('/config/numWorkers', {
         method: 'POST',
-        body: numWorkers,
+        body: numWorkersValue,
         headers: { 'Content-Type': 'application/json' }
     });
 
     await fetch('/config/maxCrawlsPerHour', {
         method: 'POST',
-        body: maxCrawls,
+        body: maxCrawlsValue,
         headers: { 'Content-Type': 'application/json' }
     });
-    
+
     // Clear the input fields
     document.getElementById('maxCrawls').value = '';
     document.getElementById('numWorkers').value = '';
 }
 
-async function getConfig() {
+async function retrieveConfiguration() {
     const response = await fetch('/config');
-    const data = await response.json();
-    document.getElementById('configOutput').textContent = JSON.stringify(data, null, 2);
+    const configData = await response.json();
+    document.getElementById('configOutput').textContent = JSON.stringify(configData, null, 2);
 }
 
-function clearConfig() {
+function clearInputFields() {
     document.getElementById('maxCrawls').value = '';
     document.getElementById('numWorkers').value = '';
 }
 
-async function getCurrentConfig() {
+async function fetchCurrentConfiguration() {
     try {
-        // Fetch data from the /get-config endpoint
         const response = await fetch('/get-config');
-
-        // If the fetch request is not successful, throw an error
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not successful');
         }
-
-        // Parse the JSON response
         const configData = await response.json();
-
-        // Update the input fields with the configuration data
         document.getElementById('numWorkers').value = configData.numWorkers;
         document.getElementById('maxCrawls').value = configData.maxCrawlsPerHour;
     } catch (error) {
-        console.error('There was a problem with the fetch operation:', error.message);
+        console.error('An error occurred during the fetch operation:', error.message);
     }
 }
 
-function Back() {
+function navigateBack() {
     window.location.href = `/`;
 }
