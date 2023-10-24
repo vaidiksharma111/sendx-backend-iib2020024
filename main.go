1bdfca8355651c7a3df4e7b6e532c778e47272d9
Vaidik Sharma <chrisharry1281@gmail.com>
2023-10-24 22:00:52 +0530

    further  handler changes

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	handlers "project-sendx/crawl-handlers"
+	middlewares "project-sendx/middlewares"
 )
 
 const (
@@ -11,20 +11,18 @@ const (
 )
 
 func main() {
-	http.HandleFunc("/", handlers.IndexHandler)
-	http.HandleFunc("/user-login", handlers.UserLoginHandler)
-	http.HandleFunc("/user-login-form", handlers.UserLoginForm)
-	http.HandleFunc("/admin-login-form", handlers.AdminLoginForm)
-	http.HandleFunc("/admin-login", handlers.AdminLoginHandler)
-	http.HandleFunc("/crawl", handlers.CrawlHandler)
-	http.HandleFunc("/config/numWorkers", handlers.UpdateNumWorkers)
-	http.HandleFunc("/config/maxCrawlsPerHour", handlers.UpdateMaxCrawlsPerHour)
-	http.HandleFunc("/config", handlers.GetConfig)
-	http.HandleFunc("/get-config", handlers.GetConfigJSON)
-
-	http.Handle("/html/", http.StripPrefix("/html/", http.FileServer(http.Dir("./html"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
 	http.Handle("/javascript/", http.StripPrefix("/javascript/", http.FileServer(http.Dir("./javascript"))))
+	http.HandleFunc("/", middlewares.IndexHandler)
+	http.HandleFunc("/admin-login-form", middlewares.AdminLoginForm)
+	http.HandleFunc("/admin-login", middlewares.AdminLoginHandler)
+	http.HandleFunc("/user-login", middlewares.UserLoginHandler)
+	http.HandleFunc("/user-login-form", middlewares.UserLoginForm)
+	http.HandleFunc("/config/numWorkers", middlewares.UpdateNumWorkers)
+	http.HandleFunc("/get-config", middlewares.GetConfigJSON)
+	http.HandleFunc("/config/maxCrawlsPerHour", middlewares.UpdateMaxCrawlsPerHour)
+	http.HandleFunc("/crawl", middlewares.CrawlHandler)
+	http.HandleFunc("/config", middlewares.GetConfig)
 
 	fmt.Println("Server Running on ", PORT)
 	http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
